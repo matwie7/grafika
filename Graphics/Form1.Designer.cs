dbe781d4f324b21d5283b264f5499ba520667461
matwie7 <mat.wie.7@gmail.com>
2015-05-05 13:15:46 +0200

    repaired

--- Graphics/Form1.Designer.cs
+++ Graphics/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
@@ -38,7 +37,6 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
-=======
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -50,35 +48,28 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
->>>>>>> 27e07b99a6ceba59f14e862cf3156a3b176e0ee1
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-<<<<<<< HEAD
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
-=======
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
->>>>>>> 27e07b99a6ceba59f14e862cf3156a3b176e0ee1
 
         }
 
         #endregion
 
-<<<<<<< HEAD
         private System.Windows.Forms.TextBox textBox1;
-=======
         private System.Windows.Forms.Button button1;
->>>>>>> 27e07b99a6ceba59f14e862cf3156a3b176e0ee1
     }
 }
 
