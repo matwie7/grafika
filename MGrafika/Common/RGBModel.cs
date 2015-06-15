8f64aad1e2f8346a45c6c44a786572ce586d6822
matwie7 <mat.wie.7@gmail.com>
2015-06-15 19:35:54 +0200

    no message

---
+++ MGrafika/Common/RGBModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MGrafika.Common
+{
+    public class RGBModel
+    {
+        public int R { get; set; }
+        public int G { get; set; }
+        public int B { get; set; }
+    }
+}
--- MGrafika/GrOperations/ColorOperations.cs
+++ MGrafika/GrOperations/ColorOperations.cs
@@ -17,6 +17,39 @@ namespace MGrafika.GrOperations
                 Value = max};
         }
 
+        public static Color ToRgb(this HSVModel hsvColor)
+        {
+            RGBModel color = new RGBModel();
+            int r = (int) (hsvColor.Hue / 60);
+            float f = hsvColor.Hue / 60 - r;
+            int a = (int)hsvColor.Value * (1 - (int)hsvColor.Saturation);
+            int b = (int)hsvColor.Value * (int)(1 - hsvColor.Saturation * f);
+            int c = (int)hsvColor.Value * (int)(1 - hsvColor.Saturation * (1 - f));
+
+            switch (r)
+            {
+                case 0:
+                    color = new RGBModel() { R = (int)hsvColor.Value, G = c, B = a};
+                    break;
+                case 1:
+                    color = new RGBModel() { R = b, G = (int)hsvColor.Value, B = a};
+                    break;
+                case 2:
+                    color = new RGBModel() { R = a, G = (int)hsvColor.Value, B = c };
+                    break;
+                case 3:
+                    color = new RGBModel() { R = a, G = b, B = (int)hsvColor.Value };
+                    break;
+                case 4:
+                    color = new RGBModel() { R = c, G = a, B = (int)hsvColor.Value };
+                    break;
+                case 5:
+                    color = new RGBModel() { R = (int)hsvColor.Value, G = a, B = b };
+                    break;
+            }
+            return Color.FromArgb(color.R, color.G, color.B);
+        }
+
         private static float GetH(this Color color)
         {
             float hue = 0;
